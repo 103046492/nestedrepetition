29422664895aca7a79dec88c4a784b69ca37474e
Samir Crljenkovic <103046492@student.swin.edu.au>
2020-04-02 14:22:21 +1100

    95% complete

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace nestedrepetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number of rows and columns you want in the half triangle");
+            String inputString = Console.ReadLine();
+            int input = int.Parse(inputString);
+
+            for(int row = input; row >= 0; row++)
+            {
+                for(int column = row; column >= 0; column++)
+                {
+                    Console.Write("X");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
